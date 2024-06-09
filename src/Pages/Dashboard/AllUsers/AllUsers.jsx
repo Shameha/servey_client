b64ca24183e91a529d8558b689f3449837b76943
shamehaera9 <shamehaera9@gmail.com>
2024-06-10 01:43:15 +0600

    added admin dashboard

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,10 +1,95 @@
 // import React from 'react';
 
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { SiLinuxserver } from "react-icons/si";
+import { IoPeopleOutline } from "react-icons/io5";
+import Swal from "sweetalert2";
+
 const AllUsers = () => {
+    const axiosSecure = useAxiosSecure();
+    const {data:users =[],refetch} = useQuery({
+        queryKey:['users'],
+        queryFn: async () =>{
+       const res = await axiosSecure.get('/users')
+       return res.data;
+        }
+    })
+
+const handleMakeAdmin = user =>[
+    
+]
+
+    const  handleDelete = user =>{
+//   console.log(user);
+Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+    
+  axiosSecure.delete(`/users/${user._id}`)
+  .then(res =>{
+    if(res.data.deletedCount >0){
+        refetch();
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success"
+      });
+    }
+  })
+    }
+  });
+    }
     return (
-        <div>
-           <h1>here all users</h1>         
+        <div >
+        <div className="flex justify-evenly my-4">
+        <h1 className="text-3xl">here all users</h1>         
+        <h1 className="text-3xl">Total users{users.length}</h1>         
         </div>
+        <div className="overflow-x-auto w-full">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>email</th>
+        <th>Role</th>
+        <th>permision Role</th>
+        {/* <th>Action</th> */}
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      {users.map((user,index)=> <tr key={user._id}>
+        <th>{index+1}</th>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+       {/* admin */}
+        <td>
+        <button onClick={()=> handleMakeAdmin(user)} className="btn btn-primary"><IoPeopleOutline /></button> 
+        </td>
+        {/* survay */}
+        <td>
+        <button className="btn btn-secondary"><SiLinuxserver /></button>
+        </td>
+        <td>
+        <button onClick={()=>handleDelete(user)} className="btn btn-accent">Delete</button>
+        </td>
+      </tr>)}
+      
+    </tbody>
+  </table>
+</div>
+        </div>
+
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,21 @@ import {
 import { router } from './Routers/Router';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider
+
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<AuthProvider>
-<HelmetProvider>
-<RouterProvider router={router} />
-</HelmetProvider>
-</AuthProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
