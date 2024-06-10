927d85bd366880473704dd268ef4a9c4f3f20021
shamehaera9 <shamehaera9@gmail.com>
2024-06-10 09:24:43 +0600

    added 401 page

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,26 @@ export const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access token')
+        console.log("interseptors",token);
+        config.headers.authorization=`bearer ${token}`;
+        return config;
+    },function(error){
+        return Promise.reject(error);
+    })
+    axiosSecure.interceptors.response.use(function(response){
+        return response;
+      },async (error)=>{
+        const status = error.response.status;
+        console.log('status error in the interceptors',status);
+        // 401 an 403 logout the user and move the user to the login page
+        if(status === 401 || status ===403){
+          await logOut();
+            navigate('/login');
+        }
+        return Promise.reject(error);
+      })
     return axiosSecure;
 };
 
